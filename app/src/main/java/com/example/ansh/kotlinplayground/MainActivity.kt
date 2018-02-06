4efcdef2f643b447911a51c92e3c861352ad6be3
Anshdeep Singh <anshdeep.1494@gmail.com>
2018-02-06 11:20:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ansh/kotlinplayground/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ansh.kotlinplayground
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        updateTextButton.setOnClickListener { welcomeTextView.text = "Hello Kotlin World!!" }
+    }
+}
